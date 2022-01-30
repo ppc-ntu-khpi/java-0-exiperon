4cb6990ab25641464540fc3322b4e2e2688ffbbd
exiperon <67110285+exiperon@users.noreply.github.com>
2022-01-30 14:57:33 +0200

    Create Quotation.java

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  private String quote = "Copy-and-Paste was programmed by programmers for programmers actually.";
+  private String author = "The community.";
+  public void display() {
+    System.out.println(quote);
+    System.out.println("---" + author);
+  }
+}
